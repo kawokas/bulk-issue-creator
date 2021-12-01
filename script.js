cf692c0916b07bad174aa4aaa3cb60036f09b71a
kawokas <usenet.kawokas.jp@gmail.com>
2021-12-01 15:15:09 +0900

    bugfix

--- script.js
+++ script.js
@@ -15,17 +15,21 @@ const md = fs.readFileSync(mdFile,'utf8')
 const issues = md.split(/^--*$/gm)
 
 const args = issues.map((issue) => {
-  const matchedTitle = issue.match(/^\s*#\s.*$/m)[0]
-  const title = matchedTitle.replace(/\r?\n/g, '').replace(/^#\s*/, '')
-  const matchedOptions = issue.match(/^\s*options:\s.*$/m)[0]
-  const options = matchedOptions.replace(/\r?\n/g, '').replace(/^options:\s*/, '')
+  const matchedTitle = issue.match(/^\s*#\s.*$/m)?.[0]
+  const title = matchedTitle?.replace(/\r?\n/g, '')?.replace(/^#\s*/, '')
+  const matchedOptions = issue.match(/^\s*options:\s.*$/m)?.[0]
+  const options = matchedOptions?.replace(/\r?\n/g, '')?.replace(/^options:\s*/, '')
   const body = issue.replace(matchedTitle, '').replace(matchedOptions, '').replace(/^(\r?\n)*/, '')
   return { title, body, options }
 })
 
 args.forEach((arg) => {
   const {title, body, options} = arg
-  let command = `gh issue create --title "${title}" --body "${body}" --repo ${repo} ${options}`
+  if(!title) {
+    console.warn('warning: title is empty. skip this issue')
+    return
+  }
+  let command = `gh issue create --title "${title}" --body "${body || ''}" --repo ${repo} ${options || ''}`
   console.log(command)
   const out = execSync(command)
   console.log(out.toString())
